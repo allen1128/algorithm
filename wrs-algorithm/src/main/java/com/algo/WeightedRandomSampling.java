456df479003cf3eb2b9386386a4433bbec9c172d
Allen Chen <as_allen1128@hotmail.com>
2017-09-15 02:54:17 +0800

    added weighted random sampling algorithm

---
+++ wrs-algorithm/src/main/java/com/algo/WeightedRandomSampling.java
@@ -0,0 +1,57 @@
+package com.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WeightedRandomSampling {
+    public List<Double> cdf;
+
+    public WeightedRandomSampling(List<Double> weights){
+        cdf = new ArrayList<Double>(weights.size());
+        calculateCdf(weights);
+    }
+
+    private void calculateCdf(List<Double> weights){
+        Double totalWeight = 0.0d;
+        for (Double weight : weights){
+            totalWeight += weight;
+        }
+
+        Double accumulated = 0.0d;
+        for (Double weight : weights){
+            cdf.add(accumulated / totalWeight);
+            accumulated += weight;
+        }
+    }
+
+    public List<Integer> getSamples(int num){
+        List<Integer> result = new ArrayList<Integer>(num);
+        Random random = new Random();
+        for (int i = 0; i < num; i++){
+            Double prob = random.nextDouble();
+            result.add(getIndex(prob));
+        }
+        return result;
+    }
+
+    public int getIndex(Double prob){
+        int start = 0;
+        int end = cdf.size() - 1;
+
+        while (start + 1 < end){
+            int mid = start + (end - start)/2;
+            if (cdf.get(mid) > prob){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        if (cdf.get(end) <= prob){
+            return end;
+        } else {
+            return start;
+        }
+    }
+}
