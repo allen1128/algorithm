3ca32208118c62aa20cf14bb2bcbb8133a06997c
Allen Chen <as_allen1128@hotmail.com>
2017-09-30 23:45:38 +0800

    restructure the code

--- src/com/xl/algo/WeightedRandomSampling.java
+++ src/com/xl/algo/WeightedRandomSampling.java
@@ -1,8 +1,9 @@
-package com.algo;
+package com.xl.algo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Collections;
 
 public class WeightedRandomSampling {
     public List<Double> cdf;
@@ -54,4 +55,31 @@ public class WeightedRandomSampling {
             return start;
         }
     }
+
+    public static void main(String[] args){
+        List<Double> weights = new ArrayList<Double>();
+        weights.add(0.1);
+        weights.add(0.2);
+        weights.add(0.3);
+        weights.add(0.4);
+        weights.add(0.7);
+
+        WeightedRandomSampling wrs = new WeightedRandomSampling(weights);
+
+        int sampleSize = 50;
+        List<Integer> indexes= wrs.getSamples(sampleSize);
+        Collections.sort(indexes);
+
+        int[] samples = new int[weights.size()];
+        for (int i = 0; i < indexes.size(); i++){
+            samples[indexes.get(i)]++;
+        }
+
+        for (int i = 0; i < samples.length; i++){
+            System.out.println(samples[i] / Double.valueOf(sampleSize));
+        }
+
+    }
+
+
 }
--- wrs-algorithm/README.md
+++
@@ -1,5 +0,0 @@
-
-Weighted Random Sampling Algorithm
-===================================
-
-Note that: application.java contains the sample data to call the algorithm.
